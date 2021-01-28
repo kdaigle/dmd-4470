29308b71c76b5d1987d1dd32c02230a290e29c57
kdaigle <kyle@digitalworkbox.com>
2021-01-27 22:53:29 -0500

    Set the background, store, and restore it

--- colorpicker/scripts/main.js
+++ colorpicker/scripts/main.js
@@ -1,5 +1,20 @@
 const colorPicker = document.getElementById("colorPicker");
+
+window.onload = function() {
+  let backgroundColor = window.localStorage.getItem("backgroundColor");
+  if(backgroundColor) {
+    setBackgroundColor(backgroundColor);
+  }
+
+  colorPicker.value = backgroundColor;
+}
+
 colorPicker.addEventListener("change", (event) => {
   console.log(event.target.value);
-  document.body.style.backgroundColor = event.target.value;
+  window.localStorage.setItem("backgroundColor", event.target.value);
+  setBackgroundColor(event.target.value);
 })
+
+function setBackgroundColor(color) {
+  document.body.style.backgroundColor = color;
+}
